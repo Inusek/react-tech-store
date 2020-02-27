3c04a00c26e72049f0460b076e874c70aaabff07
unknown <arkadiusz.micek@gmail.com>
2020-02-27 01:38:46 +0100

    Paypal dummy btn

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ProductConsumer } from "../../context";
-
-export default function CartTotals() {
+import PayPalBtn from "./PayPalBtn";
+export default function CartTotals({ history }) {
     return (
         <div className="container">
             <div className="row">
@@ -24,6 +24,11 @@ export default function CartTotals() {
                                 <h3>subtotal : ${cartSubTotal}</h3>
                                 <h3>tax : ${cartTax}</h3>
                                 <h3>total : ${cartTotal}</h3>
+                                <PayPalBtn
+                                    history={history}
+                                    cartTotal={cartTotal}
+                                    clearCart={clearCart}
+                                />
                             </div>
                         );
                     }}
